fc63f8e0d0724810752c4e544a6d1d8ae495a01f
MatDev435 <matdev435@gmail.com>
2023-07-29 19:13:39 -0300

    Adicionando um switch case no Reducer do Contexto

--- src/contexts/CycleContexts.tsx
+++ src/contexts/CycleContexts.tsx
@@ -39,30 +39,45 @@ interface CyclesState {
 export function CycleContextProvider({ children }: CyclesContextProviderProps) {
   const [cyclesState, dispatch] = useReducer(
     (state: CyclesState, action: any) => {
-      if (action.type === 'ADD_NEW_CYCLE') {
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id,
-        }
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          }
+
+        case 'INTERRUPT_CURRENT_CYCLE':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, interruptedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+
+            activeCycleId: null,
+          }
+
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId) {
+                return { ...cycle, finishedDate: new Date() }
+              } else {
+                return cycle
+              }
+            }),
+
+            activeCycleId: null,
+          }
+
+        default:
+          return state
       }
-
-      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return { ...cycle, interruptedDate: new Date() }
-            } else {
-              return cycle
-            }
-          }),
-
-          activeCycleId: null,
-        }
-      }
-
-      return state
     },
     { cycles: [], activeCycleId: null },
   )
@@ -84,16 +99,6 @@ export function CycleContextProvider({ children }: CyclesContextProviderProps) {
         activeCycleId,
       },
     })
-
-    // setCycles((state) =>
-    //   state.map((cycle) => {
-    //     if (cycle.id === activeCycleId) {
-    //       return { ...cycle, finishedDate: new Date() }
-    //     } else {
-    //       return cycle
-    //     }
-    //   }),
-    // )
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -113,7 +118,6 @@ export function CycleContextProvider({ children }: CyclesContextProviderProps) {
       },
     })
 
-    // setCycles((state) => [...state, newCycle])
     setAmountSecondPassed(0)
   }
 
